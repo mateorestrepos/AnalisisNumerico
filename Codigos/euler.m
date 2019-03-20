e49adf2e6263f1b8bb3721d0dda158988f362895
Daples <daples1@gmail.com>
2019-03-20 09:27:52 -0500

    Euler

--- Codigos/euler.m
+++ Codigos/euler.m
@@ -2,8 +2,10 @@ function x = euler(f,h,T,x0)
     N = ceil(T/h);
     x = zeros(length(x0), N);
     x(:,1) = x0;
+    t = 0;
     for k=2:N
-        x(:,k) = x(:,k-1) + h*f(x(:,k-1))';
+        x(:,k) = x(:,k-1) + h*f(t, x(:,k-1))';
+        t = t + h;
     end
 end
 
